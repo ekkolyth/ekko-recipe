6241d3bf0664c413853dbf0421eb734751b5099b
Mike Kenway <mike@mikekenway.com>
2025-12-07 00:25:15 -0500

    first commit

---
+++ apps/docs/docs/intro.md
@@ -0,0 +1,8 @@
+# Welcome to Ekko Recipe Documentation
+
+This is the documentation site for Ekko Recipe.
+
+## Getting Started
+
+Get started by exploring the documentation.
+
---
+++ apps/docs/src/pages/index.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          Ekko Recipe
+        </Heading>
+        <p className="hero__subtitle">Recipe Documentation</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      title="Home"
+      description="Ekko Recipe Documentation">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
+
